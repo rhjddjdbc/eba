cc5169c544cc957d7781fe00adf2d6dffc5d44f2
rhjddjdbc <hfufufdjdjdjd@protonmail.com>
2025-08-11 17:32:36 +0000

    Update and rename elf_info.c to sections.c

--- sections.c
+++ sections.c
@@ -5,18 +5,24 @@
 
 void print_section_headers(FILE *f, Elf64_Ehdr header) {
     Elf64_Shdr shdr;
+
     fseek(f, header.e_shoff + header.e_shentsize * header.e_shstrndx, SEEK_SET);
     fread(&shdr, 1, sizeof(shdr), f);
 
     char *shstrtab = malloc(shdr.sh_size);
+    if (!shstrtab) {
+        perror("malloc");
+        return;
+    }
+
     fseek(f, shdr.sh_offset, SEEK_SET);
     fread(shstrtab, 1, shdr.sh_size, f);
 
     fseek(f, header.e_shoff, SEEK_SET);
     for (int i = 0; i < header.e_shnum; i++) {
         fread(&shdr, 1, sizeof(shdr), f);
-        printf("Section: %-20s | Type: %u | Addr: 0x%lx\n",
-               &shstrtab[shdr.sh_name], shdr.sh_type, shdr.sh_addr);
+        printf("Section: %-20s | Type: %u | Addr: 0x%lx | Offset: 0x%lx | Size: 0x%lx\n",
+               &shstrtab[shdr.sh_name], shdr.sh_type, shdr.sh_addr, shdr.sh_offset, shdr.sh_size);
     }
 
     free(shstrtab);

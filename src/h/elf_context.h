6bece0b7d8c957018ae0f86eff73defe6e9e6925
rhjddjdbc <hfufufdjdjdjd@protonmail.com>
2026-03-30 16:24:31 +0000

    Update elf_context.h

--- src/h/elf_context.h
+++ src/h/elf_context.h
@@ -14,9 +14,22 @@ typedef struct {
     Elf64_Shdr *dynsym;
     Elf64_Shdr *dynstr;
     const char *filename;
+
+    unsigned char **section_cache;
+    size_t *section_cache_sizes;
+    int section_cache_initialized;
 } ElfContext;
 
 int  init_elf_context(ElfContext *ctx, const char *filename);
 void free_elf_context(ElfContext *ctx);
+
+void *elf_read_section_cached(ElfContext *ctx, Elf64_Shdr *sec, size_t *out_size);
+void *elf_read_section(ElfContext *ctx, Elf64_Shdr *sec, size_t *out_size);
+void *elf_read_program_headers(ElfContext *ctx, size_t *out_count);
+void *elf_read_dynamic(ElfContext *ctx, size_t *out_size);
+void *elf_read_symbols(ElfContext *ctx, Elf64_Shdr *symtab, size_t *out_count);
+char *elf_read_strings(ElfContext *ctx, Elf64_Shdr *strtab, size_t *out_size);
+long elf_save_pos(ElfContext *ctx);
+void elf_restore_pos(ElfContext *ctx, long pos);
 
 #endif

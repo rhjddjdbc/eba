ed9f2817e8ea42b2ea65af1970d5381a743cbd3b
rhjddjdbc <hfufufdjdjdjd@protonmail.com>
2026-03-30 13:51:02 +0000

    Update cfg.h

--- src/h/cfg.h
+++ src/h/cfg.h
@@ -3,9 +3,22 @@
 
 #include <capstone/capstone.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// Control Flow Graph Edge 
+typedef struct {
+    uint64_t from;
+    uint64_t to;
+    char type[32];
+} CFGEdge;
 
 int get_config_bool(const char *filename, const char *key, int default_value);
 
-void generate_cfg(cs_insn *insn, size_t count);
+size_t generate_cfg(cs_insn *insn, size_t count, CFGEdge **edges);
+
+void cfg_export_dot(FILE *out, CFGEdge *edges, size_t edge_count);
+
+void generate_cfg_legacy(cs_insn *insn, size_t count);
 
 #endif

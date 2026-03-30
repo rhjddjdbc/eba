07a18bb7634fdc4d505b15990044c0714c840cf3
rhjddjdbc <hfufufdjdjdjd@protonmail.com>
2026-03-30 16:23:51 +0000

    Update common.h

--- src/h/common.h
+++ src/h/common.h
@@ -12,4 +12,11 @@
 #define CYAN    "\x1b[36m"
 #define RESET   "\x1b[0m"
 
+#define BUFFER_SIZE     4096
+#define MAX_STRING_LEN  256
+#define MAX_STRINGS     100
+#define MAX_SYMBOLS     100
+#define MAX_PATH_LEN    4096
+#define MAX_LINE_LEN    256
+
 #endif
